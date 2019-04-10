3c07ce733360ee7f180067ed176decb012969612
Hoby Rakotoarivelo <hoby.rakotoarivelo@gmail.com>
2019-04-10 23:35:15 +0200

    Fix PAPI dependency issue

--- cmake/FindPAPI.cmake
+++ cmake/FindPAPI.cmake
@@ -1,8 +1,19 @@
 include(FindPackageHandleStandardArgs)
 
-find_path(PAPI_INCLUDE_DIR papi.h)
-find_library(PAPI_LIBRARY  papi)
-find_package_handle_standard_args(PAPI DEFAULT_MSG PAPI_INCLUDE_DIR PAPI_LIBRARY)
+find_path(PAPI_PREFIX NAMES include/papi.h)
 
-mark_as_advanced(PAPI_INCLUDE_DIR PAPI_LIBRARY)
+find_library(PAPI_LIBRARIES
+        NAMES libpapi.so libpapi.a papi
+        HINTS ${PAPI_PREFIX}/lib ${HILTIDEPS}/lib)
+
+find_path(PAPI_INCLUDE_DIRS
+        NAMES papi.h
+        HINTS ${PAPI_PREFIX}/include ${HILTIDEPS}/include)
+
+find_package_handle_standard_args(PAPI
+        DEFAULT_MSG
+        PAPI_LIBRARIES
+        PAPI_INCLUDE_DIRS)
+
+mark_as_advanced(PAPI_PREFIX_DIRS PAPI_LIBRARIES PAPI_INCLUDE_DIRS)
 
