f422f6f385851fd7b00e94520f9ecdf122fe3cd8
Hoby Rakotoarivelo <hoby.rakotoarivelo@gmail.com>
2019-04-11 00:30:30 +0200

    Remove unnecessary include paths

--- cmake/FindPAPI.cmake
+++ cmake/FindPAPI.cmake
@@ -2,13 +2,13 @@ include(FindPackageHandleStandardArgs)
 
 find_path(PAPI_PREFIX NAMES include/papi.h)
 
-find_library(PAPI_LIBRARIES
-        NAMES libpapi.so libpapi.a papi
-        HINTS ${PAPI_PREFIX}/lib ${HILTIDEPS}/lib)
-
 find_path(PAPI_INCLUDE_DIRS
         NAMES papi.h
-        HINTS ${PAPI_PREFIX}/include ${HILTIDEPS}/include)
+        HINTS ${PAPI_PREFIX}/include)
+
+find_library(PAPI_LIBRARIES
+        NAMES libpapi.so libpapi.a papi
+        HINTS ${PAPI_PREFIX}/lib)
 
 find_package_handle_standard_args(PAPI
         DEFAULT_MSG
